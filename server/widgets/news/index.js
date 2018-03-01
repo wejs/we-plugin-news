b5d10422df86f58acf7c7336d5a4c32d15e5adf6
albertosouza <alberto.souza.99@gmail.com>
2018-03-01 03:11:42 -0300

    add limit configuration in news widget

--- server/widgets/news/index.js
+++ server/widgets/news/index.js
@@ -5,30 +5,31 @@
 module.exports = function (projectPath, Widget) {
   const widget = new Widget('news', __dirname);
 
-  // // Override default widget class functions after instance
-  //
-  // widget.beforeSave = function widgetBeforeSave(req, res, next) {
-  //   // do something after save this widget in create or edit ...
-  //   return next();
-  // };
-
-  // // form middleware, use for get data for widget form
-  // widget.formMiddleware = function formMiddleware(req, res, next) {
-  //
-  //   next();
-  // }
+  widget.beforeSave = function disqusWidgetBeforeSave(req, res, next) {
+    req.body.configuration = {
+      limit: req.body.limit
+    };
+    return next();
+  };
 
   widget.viewMiddleware = function viewMiddleware(widget, req, res, next) {
-    return req.we.db.models['news']
+
+    let limit = 6;
+
+    const conf = widget.configuration;
+    if (conf && conf.limit && Number(conf.limit)) {
+      limit = conf.limit;
+    }
+
+    return req.we.db.models.news
     .findAll({
       where: { published: true },
       order: [
         ['highlighted', 'DESC'],
         ['publishedAt', 'DESC'],
-        ['createdAt', 'DESC'],
-        ['id', 'DESC']
+        ['createdAt', 'DESC']
       ],
-      limit: 4
+      limit: limit
     })
     .then( (p)=> {
       widget.news = p;
@@ -36,7 +37,7 @@ module.exports = function (projectPath, Widget) {
       return null;
     })
     .catch(next);
-  }
+  };
 
   return widget;
 };
